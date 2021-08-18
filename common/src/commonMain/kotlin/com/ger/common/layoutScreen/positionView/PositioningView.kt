41825374abc34728b1acae22e813e6c0a8a9a3a0
art241111 <art241111@mail.ru>
2021-08-18 15:04:37 +0300

    Transferring logic to another file

--- common/src/commonMain/kotlin/com/ger/common/layoutScreen/positionView/PositioningView.kt
+++ common/src/commonMain/kotlin/com/ger/common/layoutScreen/positionView/PositioningView.kt
@@ -31,74 +31,11 @@ import com.ger.common.data.Product
 import com.ger.common.layoutScreen.dragableBoxView.DragableBoxView
 import com.ger.common.layoutScreen.dragableBoxView.data.Block
 import com.ger.common.layoutScreen.dragableBoxView.data.Intersection
+import com.ger.common.layoutScreen.positionView.isInAreaCheck
+import com.ger.common.layoutScreen.positionView.isIntersectionCheck
 import com.ger.common.utils.add
 import com.ger.common.utils.change
 import com.ger.common.utils.toInt
-
-
-fun isInAreaCheck(x: Int, y: Int, block: Block, density: Density, pallet: Pallet, palletOverhang: Int): Boolean {
-    val blockOverhang = block.overhang.dp.toInt(density)
-    val blockLength = block.length.dp.toInt(density)
-    val blockWidth = block.width.dp.toInt(density)
-
-    val newX = x + blockOverhang
-    val newY = y + blockOverhang
-
-    val areaLength = (pallet.length + 2 * palletOverhang).dp.toInt(density)
-    val areaWidth = (pallet.width + 2 * palletOverhang).dp.toInt(density)
-
-    val isCross1 = ((newX + blockLength) < areaLength) && (((0 - blockWidth) < newY) && (newY < areaWidth))
-    val isCross2 = ((newY + blockWidth) < areaWidth) && (((0 - blockLength) < newX) && (newX < areaLength))
-    val isCross3 = (newX > 0) && (((0 - blockWidth) < newY) && (newY < areaWidth))
-    val isCross4 = (newY > 0) && (((0 - blockLength) < newX) && (newX < areaLength))
-
-    return isCross1 && isCross2 && isCross3 && isCross4
-}
-
-fun isIntersectionCheck(x: Int, y: Int, block: Block, density: Density, mainBlock: Block): Intersection {
-    val blockOverhang = block.overhang.dp.toInt(density)
-    val blockLength = block.length.dp.toInt(density) + (blockOverhang * 2)
-    val blockWidth = block.width.dp.toInt(density) + (blockOverhang * 2)
-
-    val mainBlockOverhang = mainBlock.overhang.dp.toInt(density)
-    val mainBlockLength = (mainBlock.length).dp.toInt(density) + 2 * mainBlockOverhang
-    val mainBlockWidth = (mainBlock.width).dp.toInt(density) + 2 * mainBlockOverhang
-
-    val isCross1 =
-        (x < (mainBlockLength + mainBlock.printX)) && (((mainBlock.printY - blockWidth) < y) && (y < (mainBlockWidth + mainBlock.printY)))
-    val isCross2 =
-        (y < (mainBlockWidth + mainBlock.printY)) && (((mainBlock.printX - blockLength) < x) && (x < (mainBlockLength + mainBlock.printX)))
-    val isCross3 =
-        ((x + blockLength) > mainBlock.printX) && (((mainBlock.printY - blockWidth) < y) && (y < (mainBlockWidth + mainBlock.printY)))
-    val isCross4 =
-        ((y + blockWidth) > mainBlock.printY) && (((mainBlock.printX - blockLength) < x) && (x < (mainBlockLength + mainBlock.printX)))
-
-    var xDistance = 0
-    var yDistance = 0
-
-    val isIntersection = isCross1 && isCross2 && isCross3 && isCross4
-
-    if (isCross1 && !isCross2 && !isCross3 && !isCross4) {
-        xDistance = (mainBlockLength + mainBlock.printX - x).toInt()
-    }
-
-    if (!isCross1 && isCross2 && !isCross3 && !isCross4) {
-        yDistance = -1
-    }
-
-    if (!isCross1 && !isCross2 && isCross3 && !isCross4) {
-        xDistance = -1
-    }
-
-    if (!isCross1 && !isCross2 && !isCross3 && isCross4) {
-        yDistance = 1
-    }
-    return Intersection(
-        isIntersection = isIntersection,
-        xDistance = xDistance,
-        yDistance = yDistance
-    )
-}
 
 @Composable
 fun ColumnScope.PositioningView(
